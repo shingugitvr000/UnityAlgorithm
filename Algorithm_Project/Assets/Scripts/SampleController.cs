44791aab8857e7174f849e2e724bd11106aa3fb9
Shingugitvr000 <singugitvr000@gmail.com>
2023-09-13 14:07:05 +0900

    Feat: "싱글톤 일반 & 제너릭 구현"
    싱글톤 테스트

---
+++ Algorithm_Project/Assets/Scripts/SampleController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleController : MonoBehaviour
+{   
+    void Start()
+    {
+        Singleton.Instance.InscreaseScore(10);
+        //Start 함수가 호출될때 Instance에 접근해서 10점을 추가 
+    }
+}
