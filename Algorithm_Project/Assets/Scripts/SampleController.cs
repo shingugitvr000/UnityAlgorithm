216a5ae5b389ff305ae49dd88ed661870230c213
Shingugitvr000 <singugitvr000@gmail.com>
2023-09-13 14:26:08 +0900

    Feat: "싱글톤 제너릭 GameManager 구현"
    싱글톤 테스트

--- Algorithm_Project/Assets/Scripts/SampleController.cs
+++ Algorithm_Project/Assets/Scripts/SampleController.cs
@@ -7,6 +7,7 @@ public class SampleController : MonoBehaviour
     void Start()
     {
         Singleton.Instance.InscreaseScore(10);
+        GameManager.instance.InscreaseScore(15);
         //Start 함수가 호출될때 Instance에 접근해서 10점을 추가 
     }
 }
